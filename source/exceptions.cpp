71ea0811fc6e9824738e187df1dc35132a6c9c4d
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2024-06-29 12:12:51 +0100

    Add stacktrace to exceptions

--- source/exceptions.cpp
+++ source/exceptions.cpp
@@ -1,22 +1,43 @@
 #include "simplesquirrel/exceptions.hpp"
 
+#include <iostream>
+
 namespace ssq {
     void Exception::generate_message(HSQUIRRELVM vm, const std::string& msg) {
         std::ostringstream out;
-        out << "Squirrel exception: " << msg << " (";
+        out << "A Squirrel exception occurred!";
+
+        if (!vm) {
+            out << " " << msg << " (no detailed info)";
+            message = out.str();
+            return;
+        }
+
+        out << "\nCall stack (most recent first):";
+
+        SQStackInfos info;
+        int i = 1;
+        while (true) {
+            if (SQ_FAILED(sq_stackinfos(vm, i, &info))) {
+                // We have reached the end of the call stack
+                break;
+            } else {
+                out << "\n" << info.source << " - " << info.funcname << " - " << info.line;
+            }
+
+            i++;
+        }
+
+        out << "\n" << msg << " (";
 
         const char* lasterr;
-        if (vm) {
-            sq_getlasterror(vm);
-            if (sq_gettype(vm, -1) != OT_STRING)
-              lasterr = "no detailed info";
-            else
-              sq_getstring(vm, -1, &lasterr);
-            sq_pop(vm, 1);
-        }
-        else {
+        sq_getlasterror(vm);
+        if (sq_gettype(vm, -1) != OT_STRING)
             lasterr = "no detailed info";
-        }
+        else
+            sq_getstring(vm, -1, &lasterr);
+        sq_pop(vm, 1);
+
         out << lasterr << ")";
 
         message = out.str();
